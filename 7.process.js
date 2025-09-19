6152ecd1e210fa81447f8152ed249c84eeadf93f
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-09-19 12:05:18 -0300

    feat: módulo global process

---
+++ 7.process.js
@@ -0,0 +1,16 @@
+// argumentos de entrada: node ./7.process.js argumento1 argumento2 ...
+console.log(process.argv);
+
+// controlar el proceso y su salida: 0 (bien) y 1 (error)
+// process.exit(0);
+
+// controlar eventos del proceso
+process.on("exit", () => {
+  // limpiar los recursos
+});
+
+// current working directory
+console.log("cwd: ", process.cwd());
+
+// platform
+console.log(process.env.NO)
