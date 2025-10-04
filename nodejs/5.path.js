d3d7c52680619ca5cedcf0c5154dce593dae39dc
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-10-04 11:36:47 -0300

    feat(path): agregar comentarios explicativos en cada ejemplo

--- nodejs/5.path.js
+++ nodejs/5.path.js
@@ -1,19 +1,22 @@
 const path = require("node:path");
 
-// barra separadora de carpetas según SO 
+// barra separadora de carpetas según SO:
+// windows -> \
+// unix -> /
 console.log(path.sep);
 
 // unir rutas con path join
 const filePath = path.join("content", "subfolder", "file.txt");
 console.log(filePath);
 
+// nombre del archivo.extensión
 const base = path.basename("/tmp/juanb/password.txt");
 console.log(base);
 
+// nombre del archivo sin extensión
 const filename = path.basename("/temp/juanb/password.txt", ".txt");
 console.log(filename);
 
+// extensión
 const extension = path.extname("image.jpg");
 console.log(extension)
-
-
